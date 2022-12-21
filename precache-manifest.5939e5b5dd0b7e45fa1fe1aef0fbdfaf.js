e38777596cc08cfcb8bd846c2e2658598f8210ca
maifeeulasad <maifeeulasad@users.noreply.github.com>
2022-12-21 05:45:57 +0000

    deploy: 04cffce3e1eeb8443a248cb33173b6d1c8e6fc92

---
+++ precache-manifest.5939e5b5dd0b7e45fa1fe1aef0fbdfaf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d62f668d18f0c9fd0dfae7af5efb90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cddd54118cdcd2eb68a",
+    "url": "/static/css/2.5a645664.chunk.css"
+  },
+  {
+    "revision": "73b59cf64b49472bfc79",
+    "url": "/static/css/main.393ebfc0.chunk.css"
+  },
+  {
+    "revision": "5cddd54118cdcd2eb68a",
+    "url": "/static/js/2.3bfe10d6.chunk.js"
+  },
+  {
+    "revision": "9ea428e410ce03ce9bd67cd4e5b30c11",
+    "url": "/static/js/2.3bfe10d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b59cf64b49472bfc79",
+    "url": "/static/js/main.b699729e.chunk.js"
+  },
+  {
+    "revision": "267cede3a7e71f7a4334",
+    "url": "/static/js/runtime-main.943a40dc.js"
+  },
+  {
+    "revision": "bc7a0d62bcdc02a57d9c7585123ece78",
+    "url": "/static/media/hug.bc7a0d62.webp"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
